40cf7bb9134e518a2c071ce71ef1e82180521c09
404LinkNotFound <404LinkNotFound@users.noreply.github.com>
2019-01-31 23:33:13 -0400

    Added source file

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main()
 {
-	cout << "hello world" ;
+	vector<int> vectorvondoom ={1,2,3,4,5}; // A new vector
+	cout << "hello world" ;// test 
+	for (int i = o; i < vectorvondoom.size() ; i++)
+	{
+		cout <<vectorvondoom[i] << " ";
+	}
+	
 }
